8ebc3f29aaa37f3ddc517b24bcce369254fc2694
Sergey Glazov <yaglazov@gmail.com>
2023-01-09 22:28:08 +0300

    * Автоматизирует обновление README

---
+++ index.js
@@ -0,0 +1,100 @@
+/**
+ * Создаёт настроение и файл `README.md`
+ * Парсит RSS-ленту постов
+ */
+const fs = require('fs')
+const Parser = require('rss-parser')
+const parser = new Parser()
+
+
+const config = {
+  site: 'https://sglazov.ru',
+  blog: 'https://sglazov.ru/notes/',
+  feed: 'https://sglazov.ru/notes/feed/',
+  posts: 3
+}
+
+
+/**
+ * Форматировалка даты
+ */
+const formatter = new Intl.DateTimeFormat('ru', {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric'
+})
+function formatRuDate(pubDate) {
+  const date = new Date(pubDate)
+  return formatter.format(date).replace(' г.', '')
+}
+
+/**
+ * Ходит за постами
+ */
+async function loadNotes()
+{
+  const feed = await parser.parseURL(config.feed)
+
+  let links = ''
+
+  feed.items.slice(0, config.posts).forEach((item) => {
+
+    /**
+     * Очень дебильное форматирование, но хочу чистый MD
+     *
+     * ;—(
+     */
+    links += `
+* [${item.title}](${item.link}) <br />
+<sup>_${formatRuDate(item.pubDate)}_</sup>`
+  })
+
+  return links
+}
+
+
+
+(async () => {
+  let notes = ''
+
+  try
+  {
+    notes = await loadNotes()
+  } catch (e) {
+    console.error(`Не удалось загрузить записи блога ${config.site}`, e)
+  }
+
+  const result = `# \`<?php echo "console.log('Hello, World!')"; // o_0\`
+I’m Sergey and I make websites.
+
+I am currently working with Laravel and Vue. I like to work with content projects: blogs, media. I like order in files, have a sense of taste and ideals of beauty in the project.
+
+## On [My Blog](${config.blog}) <sup>_(in Russian)_</sup>
+${notes}
+
+
+## Knowledge and skills as tags
+<details>
+  <summary>WAT</summary>
+
+  CSS, HTML, SCSS, PostCSS, Stylus, styled-components, Less, БЭМ, Pug (Jade), Nunjucks, JavaScript, jQuery, Vue3, a11y, Eleventy (11ty), MarkDown, Gulp, Grunt, Cypress, Git, GitHub, GitHub Actions, GitLab, Bitbucket, Sketch, Zeplin, Avacode, Photoshop, Figma, SVG, React, PHP, WordPress, Laravel _(well, a little bit)_, Flarum, Shop-Script, Bootstrap, ISPmanager, Reg.ru, TimeWeb.
+</details>
+
+----
+[github@sglazov.ru](mailto:github@sglazov.ru)
+`
+
+  /**
+   * Федеральная служба по контролю за оборотом файла `README.md`
+   */
+  fs.writeFile('README.md', result, function (err) {
+    if (err) return console.log(err)
+
+    // console.log(`${result} > README.md`)
+  })
+
+})()
+
+/**
+ * Если вам это всё надо — смело берите и используйте
+ */
